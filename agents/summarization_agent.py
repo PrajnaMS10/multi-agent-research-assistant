3d62b0739e2fa04adb7591556bb4561cfb1403fb
122Ananya <ananyashetty2650@gmail.com>
2026-04-10 07:41:39 +0530

    added groq

--- agents/summarization_agent.py
+++ agents/summarization_agent.py
@@ -1,14 +1,15 @@
-# # from openai import OpenAI
+# from openai import OpenAI
 # from google import genai
-import google.generativeai as genai
+# import google.generativeai as genai
+from groq import Groq
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
 def summarize_paper(abstract: str, title: str = "") -> dict:
     # client = OpenAI() 
-    genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-    model = genai.GenerativeModel("gemini-2.5-flash-lite")
+    # genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+    # model = genai.GenerativeModel("gemini-2.5-flash-lite")
     title_context = f'Title: "{title}"\n\n' if title else ""
 
     prompt = f"""You are a research paper summarization expert.
@@ -30,9 +31,33 @@ Provide a structured summary in the following JSON format (respond with ONLY val
     #     max_tokens=1024,
     #     messages=[{"role": "user", "content": prompt}]
     # )
-    response = model.generate_content(prompt)
+    # response = model.generate_content(prompt)
     # raw = response.text.strip()
-    raw = (response.text or "").strip()
+    # raw = (response.text or "").strip()
+
+    client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+    completion = client.chat.completions.create(
+        model="openai/gpt-oss-120b",
+        messages=[
+        {
+            "role": "user",
+            "content": prompt
+            # temperature=0.2
+            # max_tokens=1024
+        }
+        ],
+        temperature=1,
+        max_completion_tokens=1024,
+        top_p=1,
+        reasoning_effort="medium",
+        stream=False,
+        stop=None
+    )
+
+    # for chunk in completion:
+    #     print(chunk.choices[0].delta.content or "", end="")
+    raw = (completion.choices[0].message.content or "").strip()
+
 
     # Parse JSON response
     import json
